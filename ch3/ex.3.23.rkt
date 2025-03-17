458c96879d9650a45097c71c1679c3b8d9cdff22
Akari <jvjdev@gmail.com>
2025-03-17 14:54:22 +0800

    refactor deque implementation: improve error handling and enhance test coverage

--- ch3/ex.3.23.rkt
+++ ch3/ex.3.23.rkt
@@ -19,21 +19,20 @@
 (define (prev-deque-node node) (mcadr node))
 (define (next-deque-node node) (mcaddr node))
 
-(define (set-deque-node-val! node new-val)
-  (set-mcar! node new-val))
 (define (set-deque-node-prev! node new-prev)
   (set-mcar! (mcdr node) new-prev))
 (define (set-deque-node-next! node new-next)
   (set-mcar! (mcddr node) new-next))
 
 (define (empty-deque? deque) (null? (front-node-ptr deque)))
+
 (define (front-deque deque)
-  (if (null? deque)
+  (if (empty-deque? deque)
       (error "FRONT call with an empty deque" deque)
       (val-deque-node (front-node-ptr deque))))
 
 (define (rear-deque deque)
-  (if (null? deque)
+  (if (empty-deque? deque)
       (error "REAR call with a empty deque" deque)
       (val-deque-node (rear-node-ptr deque))))
 
@@ -59,18 +58,26 @@
 (define (front-delete-deque! deque)
   (cond [(empty-deque? deque)
          (error "FRONT-DELETE! called with an empty deque")]
+        [(eq? (front-node-ptr deque) (rear-node-ptr deque))
+         ; If this is the last element, clear both pointers
+         (set-front-node-ptr! deque '())
+         (set-rear-node-ptr! deque '())]
         [else
-         (set-front-node-ptr!
-          deque
-          (next-deque-node (front-node-ptr deque)))]))
+         (let ([new-front (next-deque-node (front-node-ptr deque))])
+           (set-deque-node-prev! new-front '())
+           (set-front-node-ptr! deque new-front))]))
 
 (define (rear-delete-deque! deque)
   (cond [(empty-deque? deque)
          (error "REAR-DELETE! called with an empty deque")]
+        [(eq? (front-node-ptr deque) (rear-node-ptr deque))
+         ; If this is the last element, clear both pointers
+         (set-front-node-ptr! deque '())
+         (set-rear-node-ptr! deque '())]
         [else
-         (set-rear-node-ptr!
-          deque
-          (prev-deque-node (rear-node-ptr deque)))]))
+         (let ([new-rear (prev-deque-node (rear-node-ptr deque))])
+           (set-deque-node-next! new-rear '())
+           (set-rear-node-ptr! deque new-rear))]))
 
 (define (print-deque deque)
   (define (iter node)
@@ -87,36 +94,110 @@
   (display ")"))
 
 (module+ test
-  (require support/testing)
+  (require support/testing
+           rackunit/text-ui)
 
-  (define d (make-deque))
+  (define deque-tests
+    (test-suite
+     "Tests for deque implementation"
 
-  (check-normalized-output
-   (lambda ()
-     (print-deque d) (newline)
-     (front-insert-deque! d 'b)
-     (print-deque d) (newline)
-     (front-insert-deque! d 'a)
-     (print-deque d) (newline)
-     (front-insert-deque! d 0)
-     (print-deque d) (newline)
-     (rear-insert-deque! d 'c)
-     (print-deque d) (newline)
-     (rear-insert-deque! d 'd)
-     (print-deque d) (newline)
-     (rear-insert-deque! d 0)
-     (print-deque d) (newline)
-     (rear-delete-deque! d)
-     (print-deque d) (newline)
-     (front-delete-deque! d)
-     (print-deque d) (newline))
+     (test-case "empty-deque? with new deque"
+       (let ([d (make-deque)])
+         (check-true (empty-deque? d))))
 
-   '("(deque)"
-     "(deque b)"
-     "(deque a b)"
-     "(deque 0 a b)"
-     "(deque 0 a b c)"
-     "(deque 0 a b c d)"
-     "(deque 0 a b c d 0)"
-     "(deque 0 a b c d)"
-     "(deque a b c d)")))
+     (test-case "empty-deque? after insertion and deletion"
+       (let ([d (make-deque)])
+         (front-insert-deque! d 'x)
+         (check-false (empty-deque? d))
+         (front-delete-deque! d)
+         (check-true (empty-deque? d))))
+
+     (test-case "front-insert and front-deque"
+       (let ([d (make-deque)])
+         (front-insert-deque! d 'a)
+         (check-equal? (front-deque d) 'a)
+         (front-insert-deque! d 'b)
+         (check-equal? (front-deque d) 'b)
+         (check-equal? (rear-deque d) 'a)))
+
+     (test-case "rear-insert and rear-deque"
+       (let ([d (make-deque)])
+         (rear-insert-deque! d 'a)
+         (check-equal? (rear-deque d) 'a)
+         (rear-insert-deque! d 'b)
+         (check-equal? (rear-deque d) 'b)
+         (check-equal? (front-deque d) 'a)))
+
+     (test-case "front-delete operation"
+       (let ([d (make-deque)])
+         (front-insert-deque! d 'a)
+         (front-insert-deque! d 'b)
+         (front-insert-deque! d 'c)
+         (front-delete-deque! d)
+         (check-equal? (front-deque d) 'b)))
+
+     (test-case "rear-delete operation"
+       (let ([d (make-deque)])
+         (rear-insert-deque! d 'a)
+         (rear-insert-deque! d 'b)
+         (rear-insert-deque! d 'c)
+         (rear-delete-deque! d)
+         (check-equal? (rear-deque d) 'b)))
+
+     (test-case "single element deque operations"
+       (let ([d (make-deque)])
+         (front-insert-deque! d 'x)
+         (check-equal? (front-deque d) 'x)
+         (check-equal? (rear-deque d) 'x)
+         (front-delete-deque! d)
+         (check-true (empty-deque? d))
+
+         (rear-insert-deque! d 'y)
+         (check-equal? (front-deque d) 'y)
+         (check-equal? (rear-deque d) 'y)
+         (rear-delete-deque! d)
+         (check-true (empty-deque? d))))
+
+     (test-case "error conditions"
+       (let ([d (make-deque)])
+         (check-exn exn:fail? (lambda () (front-deque d)))
+         (check-exn exn:fail? (lambda () (rear-deque d)))
+         (check-exn exn:fail? (lambda () (front-delete-deque! d)))
+         (check-exn exn:fail? (lambda () (rear-delete-deque! d)))))
+
+     (test-case "comprehensive sequence test"
+       (check-normalized-output
+        (lambda ()
+          (let ([d (make-deque)])
+            (print-deque d) (newline)
+            (front-insert-deque! d 'b)
+            (print-deque d) (newline)
+            (front-insert-deque! d 'a)
+            (print-deque d) (newline)
+            (front-insert-deque! d 0)
+            (print-deque d) (newline)
+            (rear-insert-deque! d 'c)
+            (print-deque d) (newline)
+            (rear-insert-deque! d 'd)
+            (print-deque d) (newline)
+            (rear-insert-deque! d 'e)
+            (print-deque d) (newline)
+            (front-delete-deque! d)
+            (print-deque d) (newline)
+            (rear-delete-deque! d)
+            (print-deque d) (newline)
+            (front-delete-deque! d)
+            (front-delete-deque! d)
+            (print-deque d) (newline)))
+        '("(deque)"
+          "(deque b)"
+          "(deque a b)"
+          "(deque 0 a b)"
+          "(deque 0 a b c)"
+          "(deque 0 a b c d)"
+          "(deque 0 a b c d e)"
+          "(deque a b c d e)"
+          "(deque a b c d)"
+          "(deque c d)")))))
+
+  (run-tests deque-tests))
